2be0fc5e83b814f0253bb68f947f86e5d3b23e17
rtyagi05 <111298496+rtyagi05@users.noreply.github.com>
2022-08-15 21:56:58 +0530

    Update Planet.cs
    made AverageMoonGravity as double

--- Test-Taste-Console-Application/Domain/Objects/Planet.cs
+++ Test-Taste-Console-Application/Domain/Objects/Planet.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Test_Taste_Console_Application.Domain.DataTransferObjects;
+using Test_Taste_Console_Application.Domain.Services;
 
 namespace Test_Taste_Console_Application.Domain.Objects
 {
@@ -10,28 +11,29 @@ namespace Test_Taste_Console_Application.Domain.Objects
         public string Id { get; set; }
         public float SemiMajorAxis { get; set; }
         public ICollection<Moon> Moons { get; set; }
-        public float AverageMoonGravity
-        {
-            get => 0.0f;
-        }
+        public double AverageMoonGravity
+        { get; set; }
 
         public Planet(PlanetDto planetDto)
         {
             Id = planetDto.Id;
             SemiMajorAxis = planetDto.SemiMajorAxis;
             Moons = new Collection<Moon>();
-            if(planetDto.Moons != null)
+            if (planetDto.Moons != null)
             {
                 foreach (MoonDto moonDto in planetDto.Moons)
                 {
                     Moons.Add(new Moon(moonDto));
                 }
+                AverageMoonGravity= PlanetService.CalculateAverageMoonGravity(Moons,SemiMajorAxis);
             }
+          
         }
 
         public Boolean HasMoons()
         {
             return (Moons != null && Moons.Count > 0);
         }
+        
     }
 }
